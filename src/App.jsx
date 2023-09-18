e949be2e7d74a5dd6f5d75de872255dc6b9e79ff
Nick Talledo <nicolastalledo13@gmail.com>
2023-09-18 17:37:08 -0400

    a to link in nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 import Home from "./routes/Home";
 
 function App() {
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -16,12 +17,12 @@ function Nav() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center">
+        <Link to="/" className="flex items-center">
           <img src="HFLlogo.jpeg" className="h-8 mr-3" alt="HFL Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             HFL
           </span>
-        </a>
+        </Link>
         <button
           onClick={toggleMobileMenu}
           type="button"
@@ -51,12 +52,12 @@ function Nav() {
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="/News"
+              <Link
+                to="/News"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 News
-              </a>
+              </Link>
             </li>
             <li>
               <div className="relative">
@@ -100,56 +101,56 @@ function Nav() {
                     aria-labelledby="dropdownLargeButton"
                   >
                     <li>
-                      <a
-                        href="/QBtable"
+                      <Link
+                        to="/QBtable"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         QBS
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a
-                        href="/Otable"
+                      <Link
+                        to="/Otable"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Offense
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a
-                        href="/Dtable"
+                      <Link
+                        to="/Dtable"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Defense
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>
               </div>
             </li>
             <li>
-              <a
-                href="/CareerStats"
+              <Link
+                to="/CareerStats"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Career Stats
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/Potw"
+              <Link
+                to="/Potw"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Players of the Week
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/About"
+              <Link
+                to="/About"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
@@ -163,12 +164,12 @@ function Nav() {
         >
           <ul className="p-4">
             <li>
-              <a
-                href="/News"
+              <Link
+                to="/News"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
                 News
-              </a>
+              </Link>
             </li>
             <li>
               <div className="relative">
@@ -210,56 +211,56 @@ function Nav() {
                     aria-labelledby="dropdownLargeButton"
                   >
                     <li>
-                      <a
-                        href="/QBtable"
+                      <Link
+                        to="/QBtable"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         QBS
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a
-                        href="/Otable"
+                      <Link
+                        to="/Otable"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Offense
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a
-                        href="/Dtable"
+                      <Link
+                        to="/Dtable"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Defense
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>
               </div>
             </li>
             <li>
-              <a
-                href="/CareerStats"
+              <Link
+                to="/CareerStats"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Career Stats
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/Potw"
+              <Link
+                to="/Potw"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Players of the Week
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/About"
+              <Link
+                to="/About"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
