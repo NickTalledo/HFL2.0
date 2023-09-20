1d5d27a717b50fd0b0f2171b0a6015a8a6a6098c
Nick Talledo <nicolastalledo13@gmail.com>
2023-09-20 14:21:27 -0400

    news

--- src/routes/News.jsx
+++ src/routes/News.jsx
@@ -13,14 +13,6 @@ function News() {
       >
         <div className="bg-white dark:bg-gray-800 p-4 border border-gray-300 dark:border-gray-700 rounded-md max-w-[900px] mx-6 lg:mx-0 mt-6 mb-4">
           <h1 className="text-6xl text-center font-bold">News</h1>
-          <p className="mt-4 text-sm ">
-            <strong>Week 1 9/17/2023:</strong> Wow what a week. Every team that
-            lost, lost by 3 score. I mean, not to mention the Horny Crabs.
-            Losing by almost 40 points? You need to do better than that to make
-            it in this league. It didn&apos;t help that their quarterback
-            couldn&apos;t even wake up for their game. Abysmal debut by them but
-            wow that Andrew Barra (Dragons QB) kid? Guy could throw.
-          </p>
           <p className="mt-4 text-sm">
             <strong>Week 2 9/19/2023:</strong> Fighting to not be last place,
             Horny Crabs took the win. They finally had a full squad with their
@@ -28,6 +20,14 @@ function News() {
             How about Adrian Rugova breaking the sack record which he held? 7
             sacks in one game? Are you kidding me? It&apos;s looking bleak right
             now for the Strawhats, starting off 0-2.
+          </p>
+          <p className="mt-4 text-sm ">
+            <strong>Week 1 9/17/2023:</strong> Wow what a week. Every team that
+            lost, lost by 3 score. I mean, not to mention the Horny Crabs.
+            Losing by almost 40 points? You need to do better than that to make
+            it in this league. It didn&apos;t help that their quarterback
+            couldn&apos;t even wake up for their game. Abysmal debut by them but
+            wow that Andrew Barra (Dragons QB) kid? Guy could throw.
           </p>
         </div>
       </section>
