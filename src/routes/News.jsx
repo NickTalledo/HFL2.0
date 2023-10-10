04afbb809a294ff323f1a3f78a126e2872d5cde4
Nick Talledo <nicolastalledo13@gmail.com>
2023-10-09 21:02:46 -0400

    jared gm news

--- src/routes/News.jsx
+++ src/routes/News.jsx
@@ -13,6 +13,13 @@ function News() {
       >
         <div className="bg-white dark:bg-gray-800 p-4 border border-gray-300 dark:border-gray-700 rounded-md max-w-[900px] mx-6 lg:mx-0 mt-6 mb-4">
           <h1 className="text-6xl text-center font-bold">News</h1>
+          <p className="mt-4 text-sm">
+            <strong>Week 4 10/8/2023:</strong> The Strawhats beat the undefeated
+            KillaBeez. Jared has made 3 trades rebuilding basically his entire
+            roster and boy did it work. To be the last ranked team and become
+            the only team to beat the number 1 ranked team? Jared may be general
+            manager of the year, hats off to him.
+          </p>
           <p className="mt-4 text-sm ">
             <strong>TRADE ALERT 10/2/2023:</strong> The Strawhats are now
             trading both Mark Ortega and Nico Becker to the Dragons for Richie
