9d515697ca5f06a54712c2b32069f81a045f3747
Nick Talledo <nicolastalledo13@gmail.com>
2023-11-15 01:05:53 -0500

    rankings

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,36 +11,37 @@ function Home() {
             </h1>
             <img src="HFLlogo.jpeg" alt="HFL Logo" height={350} width={350} />
             <p className="max-w-[400px] text-center text-xl text-white sm:max-w-[600px] mx-8 lg:mx-0 mb-8">
-              Week 6 brought unexpected twists and intense matches, reshaping
-              the league standings and leaving fans in awe of the unpredictable
-              nature of the games.
+              As we head into the playoffs, the league standings have set the
+              stage for some exciting postseason action. Here&apos;s a recap of
+              the playoff seeds:
+              <br /> <br />
+              1. Dragons - Dominating the league with a 7-2 record, the Dragons
+              enter the playoffs as the top seed. Their consistent performance
+              throughout the season makes them a formidable contender.
               <br />
-              The Gremlins managed to secure a close win against the Strawhats
-              with a final score of 69-60. The game was tightly contested, and
-              it all came down to the final moments. However, critical mistakes
-              by the Strawhats, including a pick-six and giving up a safety in
-              back-to-back drives, cost them the game, highlighting the
-              significance of maintaining composure under pressure.
+              2. Gremlins - Securing the second seed, the Gremlins have
+              demonstrated their strength with an impressive record. Their point
+              differential has earned them this high seed, and they&apos;ll be
+              looking to make a strong playoff run.
               <br />
-              The Dragons demonstrated their dominance by blowing out the Horny
-              Crabs with a resounding 80-47 victory. The Crabs faced early
-              challenges, leading to a change in quarterback. Despite the
-              change, they couldn&apos;t recover from the Dragons&apos; strong
-              start, showcasing the Dragons&apos; formidable offense and
-              defense.
+              3. Horny Crabs - With the same record as the Gremlins, the Horny
+              Crabs claim the third seed. They&apos;ve shown their resilience
+              and potential, and they&apos;ll be eager to prove themselves in
+              the playoffs.
               <br />
-              In a stunning upset, the KillaBeez suffered a surprising loss to
-              Blackout, ending with a score of 64-72. This defeat, coming
-              against one of the bottom-ranked teams, raised doubts about the
-              KillaBeez&apos;s consistency and credibility. The unexpected
-              results have added an element of uncertainty to the league
-              rankings, making every upcoming game a potential game-changer.
+              4. KillaBeez - Despite a challenging stretch of losses, the
+              KillaBeez manage to secure the fourth seed. They&apos;ll be
+              seeking redemption and aiming to regain their winning form in the
+              postseason.
               <br />
-              With the rankings constantly shifting, Week 7 promises even more
-              excitement and intense competition. Fans eagerly anticipate the
-              next matchups, wondering which team will emerge victorious and how
-              these unexpected outcomes will continue to shape the league&apos;s
-              narrative.
+              5. Blackout - Barely making the playoffs as the fifth seed,
+              Blackout has an opportunity to make a surprise playoff run.
+              They&apos;ll be looking to turn things around and make their mark.
+              <br /> <br />
+              With the playoffs on the horizon, anticipation is high, and fans
+              can expect intense matchups and thrilling moments as these teams
+              compete for the championship title. Stay tuned for the excitement
+              and drama of the postseason!
             </p>
           </div>
           <img
@@ -77,42 +78,27 @@ function Home() {
                           Dragons
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          5-1
+                          7-2
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          324
+                          503
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 md:px-4 font-medium">
-                          238
+                          362
                         </td>
                       </tr>
-                      <tr className="border-b border-yellow-500 bg-yellow-300 text-neutral-800">
-                        <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          KillaBeez
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          4-2
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          298
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          251
-                        </td>
-                      </tr>
-
                       <tr className="border-b border-green-500 bg-green-300 text-neutral-800">
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
                           Gremlins
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          3-3
+                          5-4
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          293
+                          477
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          270
+                          422
                         </td>
                       </tr>
                       <tr className="border-b border-red-500 bg-red-300 text-neutral-800">
@@ -120,28 +106,41 @@ function Home() {
                           Horny Crabs
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          3-3
+                          5-4
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          265
+                          416
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          295
+                          432
                         </td>
                       </tr>
-
+                      <tr className="border-b border-yellow-500 bg-yellow-300 text-neutral-800">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">
+                          KillaBeez
+                        </td>
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">
+                          4-5
+                        </td>
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">
+                          441
+                        </td>
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">
+                          452
+                        </td>
+                      </tr>
                       <tr className="border-b border-neutral-700 bg-neutral-800 text-neutral-50 dark:border-neutral-600 dark:bg-neutral-700">
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
                           Blackout
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          2-4
+                          3-6
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          307
+                          453
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          358
+                          545
                         </td>
                       </tr>
                       <tr className="border-b border-blue-500 bg-blue-300 text-neutral-800">
@@ -149,13 +148,13 @@ function Home() {
                           Strawhats
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          1-5
+                          3-6
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          267
+                          412
                         </td>
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          328
+                          475
                         </td>
                       </tr>
                     </tbody>
@@ -264,10 +263,11 @@ function Home() {
                           Week 7
                         </td>
                         <td className="whitespace-nowrap  font-medium border-r border-gray-600">
-                          TBD <br /> TBD <br /> TBD
+                          Gremlins vs Horny Crabs <br /> Dragons vs Blackout{" "}
+                          <br /> KillaBeez vs Horny Crabs
                         </td>
                         <td className="whitespace-nowrap font-medium">
-                          TBD <br /> TBD <br /> TBD
+                          49-34 <br /> 80-35 <br /> 33-54
                         </td>
                       </tr>
                       <tr className="border-b border-gray-600  text-neutral-800">
@@ -275,10 +275,11 @@ function Home() {
                           Week 8
                         </td>
                         <td className="whitespace-nowrap  font-medium border-r border-gray-600">
-                          TBD <br /> TBD <br /> TBD
+                          Horny Crabs vs Blackout <br /> KillaBeez vs Strawhats
+                          <br /> Dragons vs Gremlins
                         </td>
                         <td className="whitespace-nowrap font-medium">
-                          TBD <br /> TBD <br /> TBD
+                          63-55 <br /> 52-55 <br /> 60-43
                         </td>
                       </tr>
                       <tr className="border-b border-gray-600  text-neutral-800">
@@ -286,10 +287,12 @@ function Home() {
                           Week 9
                         </td>
                         <td className="whitespace-nowrap  font-medium border-r border-gray-600">
-                          TBD <br /> TBD <br /> TBD
+                          KillaBeez vs Gremlins
+                          <br /> Blackout vs Strawhats <br /> Dragons vs
+                          Strawhats
                         </td>
                         <td className="whitespace-nowrap font-medium">
-                          TBD <br /> TBD <br /> TBD
+                          58-92 <br /> 56-44 <br /> 39-46
                         </td>
                       </tr>
                     </tbody>
